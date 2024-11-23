03b05468cd0ac6c5df8d3c88124b8b775e9c6258
Salvador Ortega <salvadorortega505@gmail.com>
2024-11-22 21:15:58 -0500

    cambios

--- lista.py
+++ lista.py
@@ -1,24 +1,31 @@
-
 class Nodo:
     def __init__(self, dato):
         self.elemento = dato
         self.next = None
+
 class ListaEnlazada:
-    
     def __init__(self):
         self.head = None
         self.tail = None
         self.length = 0
+
     def print(self):
+        if self.length == 0:
+            print("La lista está vacía.")
+            return
         temp = self.head
-        while(temp):
-            print(temp.elemento,end = '')
-            if temp.next != None:
-                print(' -> ',end = '')
+        while temp:
+            print(temp.elemento, end='')
+            if temp.next is not None:
+                print(' -> ', end='')
             temp = temp.next
         print()
-        print(self.length )
-    def append(self,dato)->bool:
+        print("Longitud:", self.length)
+
+    def append(self, dato) -> bool:
+        if dato is None:
+            print("Error: No se puede agregar un elemento 'None' a la lista.")
+            return False
         nodo = Nodo(dato)
         if self.length == 0:
             self.head = nodo
@@ -28,7 +35,11 @@ class ListaEnlazada:
             self.tail = nodo
         self.length += 1
         return True
-    def prepend(self,dato):
+
+    def prepend(self, dato):
+        if dato is None:
+            print("Error: No se puede agregar un elemento 'None' a la lista.")
+            return
         nodo = Nodo(dato)
         if self.length == 0:
             self.head = nodo
@@ -36,14 +47,15 @@ class ListaEnlazada:
         else:
             nodo.next = self.head
             self.head = nodo
-        self.length +=1
+        self.length += 1
 
     def pop(self):
         if self.length == 0:
+            print("Error: No se puede eliminar de una lista vacía.")
             return None
         temp = self.head
         pre = self.head
-        while(temp.next is not None):
+        while temp.next is not None:
             pre = temp
             temp = temp.next
         self.tail = pre
@@ -53,8 +65,10 @@ class ListaEnlazada:
             self.head = None
             self.tail = None
         return temp.elemento
+
     def prepop(self):
         if self.length == 0:
+            print("Error: No se puede eliminar de una lista vacía.")
             return None
         temp = self.head
         self.head = self.head.next
@@ -64,26 +78,39 @@ class ListaEnlazada:
             self.head = None
             self.tail = None
         return temp.elemento
-    def find(self,dato):
+
+    def find(self, dato):
+        if dato is None:
+            print("Error: El dato proporcionado no puede ser 'None'.")
+            return -1
         c = 0
         temp = self.head
-        while(temp is not None):
+        while temp is not None:
             if temp.elemento == dato:
-                break
-            c+=1
+                return c
+            c += 1
             temp = temp.next
-        return c
-    def get(self,index):
+        print(f"El elemento '{dato}' no se encontró en la lista.")
+        return -1
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            print(f"Error: Índice {index} fuera de rango.")
+            return None
         temp = self.head
         for i in range(index):
             temp = temp.next
         return temp
-    def remove(self,index):
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            print(f"Error: Índice {index} fuera de rango.")
+            return None
         if index == 0:
             return self.prepop()
-        if self.length == index:
+        if index == self.length - 1:
             return self.pop()
-        pre = self.get(index-1)
+        pre = self.get(index - 1)
         temp = pre.next
         pre.next = temp.next
         self.length -= 1
@@ -91,29 +118,32 @@ class ListaEnlazada:
             self.head = None
             self.tail = None
         return temp.elemento
-    
-    def remove_elemento(self,dato):
-        contador = self.find(dato)
-        if contador == 0:
-            return self.prepop()
-        if contador == self.length-1:
-            return self.pop()
 
+    def remove_elemento(self, dato):
+        contador = self.find(dato)
+        if contador == -1:
+            print(f"Error: El elemento '{dato}' no existe en la lista.")
+            return None
         return self.remove(contador)
-    def insert(self,pos,dato):
+
+    def insert(self, pos, dato):
+        if pos < 0 or pos > self.length:
+            print(f"Error: Índice {pos} fuera de rango.")
+            return
+        if dato is None:
+            print("Error: No se puede insertar un elemento 'None' en la lista.")
+            return
         if pos == 0:
             return self.prepend(dato)
-        if self.length == pos:
+        if pos == self.length:
             return self.append(dato)
         nodo = Nodo(dato)
-        temp = self.get(pos-1)
+        temp = self.get(pos - 1)
         nodo.next = temp.next
         temp.next = nodo
         self.length += 1
-        
-    
-        
 
+# Ejemplo de uso:
 l = ListaEnlazada()
 l.append(1)
 l.append(2)
@@ -122,5 +152,7 @@ l.append(4)
 l.print()
 l.prepend(5)
 l.print()
-l.insert(0,12213)
+l.insert(0, 12213)
 l.print()
+l.insert(10, 42)  # Intento insertar fuera de rango
+l.append(None)    # Intento agregar un valor 'None'
